78ee1672e593b77770e00a27ddce5d28d518b43d
Brice Figureau <brice@daysofwonder.com>
2011-10-03 14:27:35 +0200

    Initial import of m2e protoc connector

---
+++ src/com/daysofwonder/tools/m2e/protoc/ProtocProjectConfigurator.java
@@ -0,0 +1,16 @@
+package com.daysofwonder.tools.m2e.protoc;
+
+import org.apache.maven.plugin.MojoExecution;
+import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
+import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
+import org.eclipse.m2e.jdt.AbstractJavaProjectConfigurator;
+
+public class ProtocProjectConfigurator extends AbstractJavaProjectConfigurator {
+	@Override
+	public AbstractBuildParticipant getBuildParticipant(
+			IMavenProjectFacade projectFacade, MojoExecution execution,
+			IPluginExecutionMetadata executionMetadata) {
+		return new ProtocBuildParticipant(execution);
+	}
+}
